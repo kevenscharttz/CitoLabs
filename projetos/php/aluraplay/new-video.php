3b4f4e9b5e3b6fb8f630e1b4da1b31226a0fcc60
kevenscharttz <kevenscha@gmail.com>
2025-07-14 17:12:29 -0300

    alterado forma de recebimento das informações melhorado segurança dos dados, melhorado validação

--- projetos/php/aluraplay/new-video.php
+++ projetos/php/aluraplay/new-video.php
@@ -2,8 +2,11 @@
 
 require "./connection.php";
 
-$url = $_POST['url'];
-$title = $_POST['titulo'];
+$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+$title = filter_input(INPUT_POST, 'title');
+if ($title === false || $url === false){
+    header("Location: ./index.php?sucesso=0");
+}
 
 $sql = "INSERT INTO videos (url, title) VALUES (?, ?)";
 
@@ -11,8 +14,9 @@ $statement = $pdo->prepare($sql);
 $statement->bindValue(1, $url);
 $statement->bindValue(2, $title);
 
-if ($statement->execute() === false) {
-    header("Location: ./index.php?successo=0");
-} else {
+try {
+    $statement->execute();
     header("Location: ./index.php?sucesso=1");
+} catch (PDOException) {
+    header("Location: ./index.php?sucesso=0");
 }
