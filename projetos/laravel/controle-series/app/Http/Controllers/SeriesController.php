dde9524c5bb51951561f32960907f4bc018502db
Keven Willians <149530293+kevenscharttz@users.noreply.github.com>
2025-08-03 02:41:35 -0300

    refactor: alterado código da view para se adequar ao uso do blade, e altedado forma de envio dá variavel para a view

--- projetos/laravel/controle-series/app/Http/Controllers/SeriesController.php
+++ projetos/laravel/controle-series/app/Http/Controllers/SeriesController.php
@@ -16,9 +16,7 @@ class SeriesController extends Controller
             'The boys',
         ];
 
-        return view(
-            'listar-series',
-            compact('series')
-        );
+        return view('listar-series')
+            ->with('series', $series);
     }
 }
