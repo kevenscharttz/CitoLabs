1b08ad15d17b681907cfbdd13bd8439b1d6fbd32
keven <kevenscha@gmail.com>
2025-08-06 16:24:35 -0300

    rebase: Feito um agrupamento das actions

--- projetos/laravel/controle-series/routes/web.php
+++ projetos/laravel/controle-series/routes/web.php
@@ -4,11 +4,14 @@ use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/series');
 });
 
-Route::get('/series', [SeriesController::class, 'index']);
 
-Route::get('/series/criar', [SeriesController::class, 'create']);
+Route::controller(Seriescontroller::class)->group(function(){
+    Route::get('/series', ['index']);
 
-Route::post('/series/salvar', [SeriesController::class, 'store']);
+    Route::get('/series/criar', ['create']);
+
+    Route::post('/series/salvar', ['store']);
+});
