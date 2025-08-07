63755875aeb73dd6612d52de7efcfaa31b92f12c
keven <kevenscha@gmail.com>
2025-08-07 17:08:16 -0300

    feat: nova feature para exclusão das séries

--- projetos/laravel/controle-series/resources/views/series/index.blade.php
+++ projetos/laravel/controle-series/resources/views/series/index.blade.php
@@ -1,9 +1,19 @@
 <x-layout title="Séries">
-    <a class="btn btn-primary" href="{{ route('series.create')}}" role="button">Link</a>
-    
+    <a class="btn btn-primary" href="{{ route('series.create') }}" role="button">Link</a>
+
     <ul class="list-group">
         @foreach ($series as $serie)
-            <li class="list-group-item">{{ $serie->nome }}</li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                {{ $serie->nome }}
+                
+                <form action="{{ route('series.destroy', $serie->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-sm">
+                        X
+                    </button>
+                </form>
+            </li>
         @endforeach
     </ul>
 </x-layout>
