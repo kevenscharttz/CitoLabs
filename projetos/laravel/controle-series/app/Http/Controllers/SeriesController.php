7ed56bc310fca610e2bc061fa51337daff7f061b
keven <kevenscha@gmail.com>
2025-08-12 16:52:46 -0300

    feat: criado pastas para linguas e modificado mensagens de validação

--- projetos/laravel/controle-series/app/Http/Controllers/SeriesController.php
+++ projetos/laravel/controle-series/app/Http/Controllers/SeriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use App\Http\Requests\SeriesFormRequest;
 use Illuminate\Support\Facades\DB;
 use App\Models\Serie;
 
@@ -11,18 +12,13 @@ class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-
         $series = Serie::query()
             ->orderBy('nome')
             ->get();
-
         $mensagemSucesso = $request->session()->get('mensagem.sucesso');
-
         return view('series.index')
             ->with('series', $series)
             ->with('mensagemSucesso', $mensagemSucesso);
-
-        
     }
 
     public function create()
@@ -30,21 +26,15 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
-        $request->validate([
-            'nome' => ['required', 'min:3']
-        ]);
         $nomeSerie = Serie::create($request->all());
-
         $request->session()->flash('mensagem.sucesso', "{$nomeSerie -> nome} adiciona com sucesso");
         return to_route('series.index');
-        
     }
 
-    public function destroy(Serie $series, Request $request)
+    public function destroy(Serie $series)
     {
-
         $series->delete();
         return to_route('series.index')
         ->with('mensagem.sucesso', "{$series -> nome} removida com sucesso");
@@ -55,11 +45,10 @@ class SeriesController extends Controller
         return view('series.edit')->with('series', $series);
     }
 
-    public function update(Serie $series, Request $request)
+    public function update(Serie $series, SeriesFormRequest $request)
     {   
         $series->fill($request->all());
         $series->save();
-
         return to_route('series.index')
         ->with('mensagem.sucesso', "{$series -> nome} atualizada com sucesso");
     }
