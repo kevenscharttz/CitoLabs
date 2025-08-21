10cff6ca399a2c50a6151b8785f71b7cde15e1ff
keven <kevenscha@gmail.com>
2025-08-21 18:01:04 -0300

    chore: removido arquivos  para inicio do projeto

--- projetos/laravel/note/app/Http/Controllers/MainController.php
+++ projetos/laravel/note/app/Http/Controllers/MainController.php
@@ -6,19 +6,5 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    function index($value)
-    {
-        return view('main', ['value' => $value]);
 
-        //return view('main')
-        //    ->with('value', $value);
-    }
-
-    function page1($value){
-        return view ('page1', ['value' => $value]);
-    }
-
-    function page2($value){
-        return view ('page2', ['value' => $value]);
-    }
 }
--- projetos/laravel/note/routes/web.php
+++ projetos/laravel/note/routes/web.php
@@ -2,15 +2,3 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/sobre', function () {
-   echo 'É sobre isso e tá tudo bem';
-});
-
-Route::get('/main/{value}',[MainController::class, 'index']);
-Route::get('/page1/{value}',[MainController::class, 'page1']);
-Route::get('/page2/{value}',[MainController::class, 'page2']);
