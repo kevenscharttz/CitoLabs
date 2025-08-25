dea33dcfaa4d38b049335577a9363adf0cc519bb
keven <kevenscha@gmail.com>
2025-08-25 17:58:55 -0300

    rebase: alterado mensagens padrão de erro para versões em portugûes

--- projetos/laravel/note/app/Http/Controllers/AuthController.php
+++ projetos/laravel/note/app/Http/Controllers/AuthController.php
@@ -20,9 +20,21 @@ class AuthController extends Controller
     {
         //form validation
         $request->validate(
+
+            // rules
             [
-                'text_username' => ['required', 'min:3', 'max:25'],
+                'text_username' => ['required', 'min:6', 'email'],
                 'text_password' => ['required', 'min:6', 'max:25'],
+            ],
+            // errors messages
+            [
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'Insira um e-mail válido',
+                'text_username.min' => 'O username deve possuir ao menos 6 caracteres',
+
+                'text_password.required' => 'A sennha é obrigatório',
+                'text_password.max' => 'A senha pode ter no máximo :max caracteres',
+                'text_password.min' => 'A senha deve possuir ao menos :min caracteres',
             ]
         );
 
