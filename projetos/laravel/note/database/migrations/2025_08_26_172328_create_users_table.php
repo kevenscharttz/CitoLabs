b2ce77f967a572f98ff9c3d124c1b899cb10ba51
keven <kevenscha@gmail.com>
2025-08-26 15:49:57 -0300

    feat: criado migrations para o banco

---
+++ projetos/laravel/note/database/migrations/2025_08_26_172328_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+
+            $table->id()->autoIncrement();
+
+            // nullable = can be null
+            $table->string('username', 50)->nullable();
+            $table->string('password', 200)->nullable();
+            $table->datetime('last_login')->nullable();
+
+            // created_at, updated_at
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
