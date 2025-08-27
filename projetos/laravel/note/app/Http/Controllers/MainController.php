be1b7ea648929dce026ddf2f7b2ad730f2ac1c08
keven <kevenscha@gmail.com>
2025-08-27 18:02:05 -0300

    feat: criado middlewares para autneticação do usuário

--- projetos/laravel/note/app/Http/Controllers/MainController.php
+++ projetos/laravel/note/app/Http/Controllers/MainController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    function index()
+    {
+        echo "I'm inside the app !";
+    }
 
+    function newNote()
+    {
+        echo "I'm creating a new note";
+    }
 }
--- projetos/laravel/note/routes/web.php
+++ projetos/laravel/note/routes/web.php
@@ -2,9 +2,21 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckLogged;
+use App\Http\Middleware\CheckNotLogged;
 use Illuminate\Support\Facades\Route;
 
-//Auth routes
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
-Route::get('/logout', [AuthController::class, 'logout']);
+//Auth routes - user not logged
+Route::middleware([CheckNotLogged::class])->group(function(){
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
+
+// app routes - user logged
+Route::middleware([CheckLogged::class])->group(function(){
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/newNote', [MainController::class, 'newNote']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+
