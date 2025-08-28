270f2ea9b252dd87ee6404067da21b380db7a088
keven <kevenscha@gmail.com>
2025-08-28 18:02:35 -0300

    feat: separada barra do topo da navegação, carregado as notas do usuario logado e apresentado as notas do user logado

--- projetos/laravel/note/app/Http/Controllers/MainController.php
+++ projetos/laravel/note/app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -9,10 +10,11 @@ class MainController extends Controller
     function index()
     {
         // load user's notes
+        $id = session('user.id');
+        $notes = User::find($id)->notes()->get()->toArray();
 
         //show home view
-
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
 
     function newNote()
